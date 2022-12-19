51df6edd1449209f13631b493f2c5db032cacc5e
OmarSuarezDoro <alu0101483474@ull.edu.es>
2022-12-19 21:13:04 +0000

    updates

--- mochila/ProgramInterface.cc
+++ mochila/ProgramInterface.cc
@@ -50,7 +50,7 @@ std::ostream& operator<<(std::ostream& os, const ProgramInterface& program) {
   std::vector<double> test2 = {20, 30, 66, 40, 60};
   int counter{0};
   for (const auto& element : program.Bag(program.max_weigth_, test1, test2)) {
-    std::cout << counter << element << std::endl;
+    std::cout << counter << " -> " << element << std::endl;
     counter++;
   }
   return os;
@@ -58,18 +58,25 @@ std::ostream& operator<<(std::ostream& os, const ProgramInterface& program) {
 
 
 std::vector<double> ProgramInterface::Bag(double max_weight, const std::vector<double>& weigth_vector, const std::vector<double>& value_vector) const {
-  std::vector<double> result(0, weigth_vector.size());
+  std::vector<double> result(weigth_vector.size(), 0);
+  std::vector<double> value_vector_copy = value_vector;
   double weight{0};
-  int i{0};
-  int pos{0};
+  int i;
+  int pos;
+  int size_value_vector{value_vector.size()};
   double max_val;
   while (weight < max_weight) {
-    for (i = 0; i < value_vector.size(); ++i) {
-      value_vector[i] > max_val ? pos = i, max_val = value_vector[i] : pos, max_val;
+    max_val = 0;
+    for (i = 0; i < size_value_vector; ++i) {
+      if ((value_vector_copy[i] / weigth_vector[i]) > max_val) {
+        pos = i;
+        max_val = value_vector_copy[i] / weigth_vector[i];
+      }
     }
     if (weight + weigth_vector[pos] <= max_weight) {
-      result[i] = 1; 
+      result[pos] = 1; 
       weight += weigth_vector[pos];
+      value_vector_copy[pos] = 0;
     } else {
       result[pos] = (max_weight - weight) / weigth_vector[pos];
       weight = max_weight;
@@ -77,3 +84,33 @@ std::vector<double> ProgramInterface::Bag(double max_weight, const std::vector<d
   }
   return result;
 }
+
+/*
+std::vector<double> ProgramInterface::Bag(double max_weight, const std::vector<double>& weigth_vector, const std::vector<double>& value_vector) const {
+  std::vector<double> result(weigth_vector.size(), 0);
+  std::vector<double> value_vector_copy = value_vector;
+  double weight{0};
+  int i;
+  int pos;
+  int size_value_vector{value_vector.size()};
+  double max_val;
+  while (weight < max_weight) {
+    max_val = 0;
+    for (i = 0; i < size_value_vector; ++i) {
+      if (value_vector_copy[i] > max_val) {
+        pos = i;
+        max_val = value_vector_copy[i];
+      }
+    }
+    if (weight + weigth_vector[pos] <= max_weight) {
+      result[pos] = 1; 
+      weight += weigth_vector[pos];
+      value_vector_copy[pos] = 0;
+    } else {
+      result[pos] = (max_weight - weight) / weigth_vector[pos];
+      weight = max_weight;
+    }
+  }
+  return result;
+}
+*/
